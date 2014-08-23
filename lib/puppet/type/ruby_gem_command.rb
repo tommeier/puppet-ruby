fe288928f86d69575925b03ae615867e65c6a7c2
Tom Meier <tom@venombytes.com>
2014-08-23 12:23:10 +1000

    With type set

---
+++ lib/puppet/type/ruby_gem_command.rb
@@ -0,0 +1,41 @@
+Puppet::Type.newtype(:ruby_gem_command) do
+  @doc = ""
+
+  ensurable do
+    newvalue :present do
+      provider.create
+    end
+
+    defaultto :present
+  end
+
+  newparam(:gem) do
+    validate do |v|
+      unless v.is_a? String
+        raise Puppet::ParseError,
+          "Expected gem to be a String, got a #{v.class.name}"
+      end
+    end
+  end
+
+  newparam(:ruby_version) do
+    validate do |v|
+      unless v.is_a? String
+        raise Puppet::ParseError,
+          "Expected ruby_version to be a String, got a #{v.class.name}"
+      end
+    end
+  end
+
+  autorequire :ruby do
+    if @parameters.include?(:ruby_version) && ruby_version = @parameters[:ruby_version].to_s
+      if ruby_version == "*"
+        catalog.resources.find_all { |resource| resource.type == 'Ruby' }
+      else
+        Array.new.tap do |a|
+          a << ruby_version if catalog.resource(:ruby, ruby_version)
+        end
+      end
+    end
+  end
+end
